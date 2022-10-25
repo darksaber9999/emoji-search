03d3848e2ff36660043c4d92687dee627410d03c
Chris <77562903+darksaber9999@users.noreply.github.com>
2022-10-24 22:44:41 -0600

    Add copyToClipboard functionality

--- src/Components/EmojiRow.jsx
+++ src/Components/EmojiRow.jsx
@@ -1,7 +1,22 @@
 function EmojiRow(props) {
   const { title, symbol, keywords } = props.emoji;
+
+  const copyToClipboard = async (event) => {
+    if (!navigator.clipboard) {
+      // Clipboard API not available
+      console.log("API not available");
+      return
+    }
+    const copiedEmoji = event.target.children[0].innerHTML;
+    try {
+      await navigator.clipboard.writeText(copiedEmoji)
+    } catch (err) {
+      console.error('Failed to copy!', err)
+    }
+  }
+
   return (
-    <div className="emojiRow" keywords={keywords}>
+    <div className="emojiRow" keywords={keywords} onClick={copyToClipboard}>
       <span>{symbol}</span>
       <span>{title}</span>
     </div>
